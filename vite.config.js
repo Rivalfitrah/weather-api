3061e4f240dfd5ed9b1d4b642699e045054aa1d4
rival fitrah <rivalfitrah9@gmail.com>
2025-07-10 18:49:35 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom']
-        }
-      }
-    }
-  },
-  define: {
-    global: 'globalThis'
-  }
+  plugins: [react(),
+    tailwindcss()
+  ]
 })
