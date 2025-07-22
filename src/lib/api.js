0fd39e1cbaa6191dedcae255c32e08e1e983b9da
rival fitrah <rivalfitrah9@gmail.com>
2025-07-22 19:27:02 +0700

    update web

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,6 +3,8 @@ import axios from "axios"
 const baseUrl = "http://api.weatherapi.com/v1"
 const apiKey = import.meta.env.VITE_WEATHER_API_KEY
 
+
+// function untuk get cuaca
 export const getcuaca =async (city) => {
     try {
         const response = await axios.get(`${baseUrl}/current.json`, {
